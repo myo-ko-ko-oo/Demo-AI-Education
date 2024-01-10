6b0ef15316003dd2ad52c5690ca04876bd5dd706
MYO KO KO OO <myokokooo.5287@gmail.com>
2024-01-11 05:07:49 +0630

    vite  update

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename="/Demo-AI-Education"  hashType="noslash">
+    <BrowserRouter basename="/Demo-AI-Education/"  hashType="noslash">
       <App />
     </BrowserRouter>
   </React.StrictMode>
