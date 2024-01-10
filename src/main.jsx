7630811f6e5e2a1c7f5dbd1a4df2b7b286629410
MYO KO KO OO <myokokooo.5287@gmail.com>
2024-01-11 04:15:39 +0630

    reponame  update

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename="/your-repo-name">
+    <BrowserRouter basename="/Demo-AI-Education">
       <App />
     </BrowserRouter>
   </React.StrictMode>
