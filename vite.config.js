601d5ad194a2ffd736ad9eb71a7a2dd35e67ca74
MYO KO KO OO <myokokooo.5287@gmail.com>
2024-01-11 04:25:48 +0630

    vite  update

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"AI-REACT-UI",
+  base:"/Demo-AI-Education/",
   plugins: [react()],
   build: {
     chunkSizeWarningLimit: 1000, // Set your desired limit
